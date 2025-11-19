9784c6175c3b71125a71df8f44470cf7c99fa21c
Sanjeev Midda <s.midda1@gmail.com>
2025-11-19 14:26:50 +0000

    added actions for delete and checkbox

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Task = ({ taskItem }) => {
   console.log(taskItem);
 
-  return <div className="taskContainer">{taskItem}</div>;
+  return (
+    <div className="taskContainer">
+      <p>D</p>
+      <li>{taskItem}</li>
+
+      <div className="actions">
+        <button>🗑️</button>
+
+        <label>
+          <input type="checkbox"></input>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
