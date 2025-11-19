77c999f783875028b4459808caf872a05c99bf4d
Sanjeev Midda <s.midda1@gmail.com>
2025-11-19 14:10:16 +0000

    created todo items in an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./index.css";
 
 function App() {
+  let tasks = [
+    "Buy milk",
+    "Read a book",
+    "Collect parcel",
+    "Book flights to Rome",
+  ];
+
   return (
     <>
       <header>
         <h1>To</h1>
         <h1>do</h1>
       </header>
-      <div className="mainContent"></div>
+      <div className="mainContent">
+        <h2>My To-Do List</h2>
+
+        <div className="listContainer"></div>
+      </div>
     </>
   );
 }
