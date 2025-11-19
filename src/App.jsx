d19464bc725ba752c157e98fdc70bbd56a409b34
Sanjeev Midda <s.midda1@gmail.com>
2025-11-19 14:04:17 +0000

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>To</h1>
+        <h1>do</h1>
+      </header>
+      <div className="mainContent"></div>
+    </>
+  );
 }
 
 export default App;
